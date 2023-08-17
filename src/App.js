6c7759d339b893a227e4ae5b07f48db7eb4d0024
inivisibit <jon.vincent@spyrafall.com>
2023-08-16 22:10:22 -0600

    Working Spotify log in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // var client_id = new URLSearchParams(['client_id']['5c37b6f4c90143908b11d9e1727db5e7']);
+  // var redirect_uri = new URLSearchParams('http://localhost:4000/v1/spotify/callback');
+
+  // var app = express();
+
+  window.location.replace('https://accounts.spotify.com/authorize?' +
+    'response_type=code' +
+    '&client_id=5c37b6f4c90143908b11d9e1727db5e7' +
+    '&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fv1%2Fspotify%2Fcallback' +
+    '&state=state' +
+    '&scope=user-follow-read');
+  // stringify({
+  //   response_type: "code",
+  //   client_id: "5c37b6f4c90143908b11d9e1727db5e7",
+  //   // scope: "scope",
+  //   redirect_uri: redirect_uri,
+  //   state: "state"
+  // }));
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +38,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
